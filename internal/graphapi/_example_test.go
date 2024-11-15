82f9d0df3a3f3724be0f5a7aafe4f1a112f1806d
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-11-15 16:41:31 -0700

    Add example test template
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

---
+++ internal/graphapi/_example_test.go
@@ -0,0 +1,407 @@
+package graphapi_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/theopenlane/utils/ulids"
+
+	"github.com/theopenlane/core/pkg/openlaneclient"
+)
+
+// This file provides examples for testing the basic CRUD operations for the Openlane API for a given object
+// you can start by replacing `OBJECT` with the object you are testing, e.g. `OBJECT`
+// for org owned objects, the setup should be pretty similar to the examples below
+// for objects with complex relationships, you may need to create additional objects to test the relationships
+
+// Before getting started, you'll need to add an OBJECTBuilder to the models_test.go file
+// and add the necessary fields to the OBJECTBuilder struct
+// Example:
+// type OBJECTBuilder struct {
+// 	client *client
+
+// 	// Fields
+// 	Name string
+// }
+
+// // MustNew OBJECT builder is used to create, without authz checks, OBJECTs in the database
+// func (e *OBJECTBuilder) MustNew(ctx context.Context, t *testing.T) *ent.OBJECT {
+// 	ctx = privacy.DecisionContext(ctx, privacy.Allow)
+
+// 	if e.Name == "" {
+// 		e.Name = gofakeit.AppName()
+// 	}
+
+// 	OBJECT := e.client.db.OBJECT.Create().
+// 		SetName(e.Name).
+// 		SaveX(ctx)
+
+// 	return OBJECT
+// }
+
+func (suite *GraphTestSuite) TestQueryOBJECT() {
+	t := suite.T()
+
+	// create an OBJECT to be queried using testUser1
+	OBJECT := (&OBJECTBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+
+	// add basic test cases for querying the OBJECT
+	testCases := []struct {
+		name     string
+		queryID  string
+		client   *openlaneclient.OpenlaneClient
+		ctx      context.Context
+		errorMsg string
+	}{
+		{
+			name:    "happy path",
+			queryID: OBJECT.ID,
+			client:  suite.client.api,
+			ctx:     testUser1.UserCtx,
+		},
+		{
+			name:    "happy path, read only user",
+			queryID: OBJECT.ID,
+			client:  suite.client.api,
+			ctx:     viewOnlyUser.UserCtx,
+		},
+		{
+			name:    "happy path using personal access token",
+			queryID: OBJECT.ID,
+			client:  suite.client.apiWithPAT,
+			ctx:     context.Background(),
+		},
+		{
+			name:     "OBJECT not found, invalid ID",
+			queryID:  "invalid",
+			client:   suite.client.api,
+			ctx:      testUser1.UserCtx,
+			errorMsg: notFoundErrorMsg,
+		},
+		{
+			name:     "OBJECT not found, using not authorized user",
+			queryID:  OBJECT.ID,
+			client:   suite.client.api,
+			ctx:      testUser2.UserCtx,
+			errorMsg: notFoundErrorMsg,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("Get "+tc.name, func(t *testing.T) {
+			resp, err := tc.client.GetOBJECTByID(tc.ctx, tc.queryID)
+
+			if tc.errorMsg != "" {
+				require.Error(t, err)
+				assert.ErrorContains(t, err, tc.errorMsg)
+				assert.Nil(t, resp)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+
+			assert.Equal(t, tc.queryID, resp.Object.ID)
+			assert.NotEmpty(t, resp.Object)
+
+			// add additional assertions for the object
+		})
+	}
+}
+
+func (suite *GraphTestSuite) TestQueryOBJECTs() {
+	t := suite.T()
+
+	// create multiple objects to be queried using testUser1
+	(&OBJECTBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+	(&OBJECTBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+
+	testCases := []struct {
+		name            string
+		client          *openlaneclient.OpenlaneClient
+		ctx             context.Context
+		expectedResults int
+	}{
+		{
+			name:            "happy path",
+			client:          suite.client.api,
+			ctx:             testUser1.UserCtx,
+			expectedResults: 2,
+		},
+		{
+			name:            "happy path, using read only user of the same org",
+			client:          suite.client.api,
+			ctx:             viewOnlyUser.UserCtx,
+			expectedResults: 2,
+		},
+		{
+			name:            "happy path, using api token",
+			client:          suite.client.apiWithToken,
+			ctx:             context.Background(),
+			expectedResults: 2,
+		},
+		{
+			name:            "happy path, using pat",
+			client:          suite.client.apiWithPAT,
+			ctx:             context.Background(),
+			expectedResults: 2,
+		},
+		{
+			name:            "another user, no OBJECTs should be returned",
+			client:          suite.client.api,
+			ctx:             testUser2.UserCtx,
+			expectedResults: 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("List "+tc.name, func(t *testing.T) {
+			resp, err := tc.client.GetAllOBJECTs(tc.ctx)
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+
+			assert.Len(t, resp.OBJECTs.Edges, tc.expectedResults)
+		})
+	}
+}
+
+func (suite *GraphTestSuite) TestMutationCreateOBJECT() {
+	t := suite.T()
+
+	testCases := []struct {
+		name        string
+		request     openlaneclient.CreateOBJECTInput
+		client      *openlaneclient.OpenlaneClient
+		ctx         context.Context
+		expectedErr string
+	}{
+		{
+			name:    "happy path, minimal input",
+			request: openlaneclient.CreateOBJECTInput{
+				// add minimal input for the OBJECT
+			},
+			client: suite.client.api,
+			ctx:    testUser1.UserCtx,
+		},
+		{
+			name:    "happy path, all input",
+			request: openlaneclient.CreateOBJECTInput{
+				// add all input for the OBJECT
+			},
+			client: suite.client.api,
+			ctx:    testUser1.UserCtx,
+		},
+		{
+			name:    "happy path, using pat",
+			request: openlaneclient.CreateOBJECTInput{
+				// add input for the OBJECT
+			},
+			client: suite.client.apiWithPAT,
+			ctx:    context.Background(),
+		},
+		{
+			name:    "happy path, using api token",
+			request: openlaneclient.CreateOBJECTInput{
+				// add input for the OBJECT
+			},
+			client: suite.client.apiWithPAT,
+			ctx:    context.Background(),
+		},
+		{
+			name:    "user not authorized",
+			request: openlaneclient.CreateOBJECTInput{
+				// add all input for the OBJECT
+			},
+			client:   suite.client.api,
+			ctx:      testUser2.UserCtx,
+			errorMsg: notauthorizedErrorMsg,
+		},
+		{
+			name:    "user not authorized, not enough permissions",
+			request: openlaneclient.CreateOBJECTInput{
+				// add all input for the OBJECT
+			},
+			client:   suite.client.api,
+			ctx:      viewOnlyUser.UserCtx,
+			errorMsg: notAuthorizedErrorMsg,
+		},
+		{
+			name:    "user not authorized, no permissions",
+			request: openlaneclient.CreateOBJECTInput{
+				// add all input for the OBJECT
+			},
+			client:   suite.client.api,
+			ctx:      viewOnlyUser.UserCtx,
+			errorMsg: notFoundErrorMsg,
+		},
+		// add additional test cases for the OBJECT
+		//   - add test cases for required fields not being provided
+		//   - add test cases for invalid input
+		{
+			name:        "missing required field",
+			request:     openlaneclient.CreateOBJECTInput{},
+			client:      suite.client.api,
+			ctx:         testUser1.UserCtx,
+			expectedErr: "value is less than the required length",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("Create "+tc.name, func(t *testing.T) {
+			resp, err := tc.client.CreateOBJECT(tc.ctx, tc.request)
+			if tc.expectedErr != "" {
+				require.Error(t, err)
+				assert.ErrorContains(t, err, tc.expectedErr)
+				assert.Nil(t, resp)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+
+			// check required fields
+
+			// check optional fields with if checks if they were provided or not
+		})
+	}
+}
+
+func (suite *GraphTestSuite) TestMutationUpdateOBJECT() {
+	t := suite.T()
+
+	OBJECT := (&OBJECTBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+	OBJECT := (&GroupBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+
+	testCases := []struct {
+		name        string
+		request     openlaneclient.UpdateOBJECTInput
+		client      *openlaneclient.OpenlaneClient
+		ctx         context.Context
+		expectedErr string
+	}{
+		{
+			name:    "happy path, update field",
+			request: openlaneclient.UpdateOBJECTInput{
+				// add field to update
+			},
+			client: suite.client.api,
+			ctx:    testUser1.UserCtx,
+		},
+		{
+			name:    "happy path, update multiple fields",
+			request: openlaneclient.UpdateOBJECTInput{
+				// add fields to update
+			},
+			client: suite.client.apiWithPAT,
+			ctx:    context.Background(),
+		},
+		// add additional test update cases for the OBJECT
+		{
+			name:    "update not allowed, not enough permissions",
+			request: openlaneclient.UpdateOBJECTInput{
+				// add field to update
+			},
+			client:   suite.client.api,
+			ctx:      viewOnlyUser.UserCtx,
+			errorMsg: notAuthorizedErrorMsg,
+		},
+		{
+			name:    "update not allowed, no permissions",
+			request: openlaneclient.UpdateOBJECTInput{
+				// add field to update
+			},
+			client:   suite.client.api,
+			ctx:      testUser2.UserCtx,
+			errorMsg: notFoundErrorMsg,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("Update "+tc.name, func(t *testing.T) {
+			resp, err := tc.client.UpdateOBJECT(tc.ctx, OBJECT.ID, tc.request)
+			if tc.expectedErr != "" {
+				require.Error(t, err)
+				assert.ErrorContains(t, err, tc.expectedErr)
+				assert.Nil(t, resp)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+
+			// add checks for the updated fields if they were set in the request
+		})
+	}
+}
+
+func (suite *GraphTestSuite) TestMutationDeleteOBJECT() {
+	t := suite.T()
+
+	// create objects to be deleted
+	OBJECT1 := (&OBJECTBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+	OBJECT2 := (&OBJECTBuilder{client: suite.client}).MustNew(testUser1.UserCtx, t)
+
+	testCases := []struct {
+		name        string
+		idToDelete  string
+		client      *openlaneclient.OpenlaneClient
+		ctx         context.Context
+		expectedErr string
+	}{
+		{
+			name:        "not authorized, delete OBJECT",
+			idToDelete:  OBJECT1.ID,
+			client:      suite.client.api,
+			ctx:         testUser2.UserCtx,
+			expectedErr: notFoundErrorMsg,
+		},
+		{
+			name:       "happy path, delete OBJECT",
+			idToDelete: OBJECT1.ID,
+			client:     suite.client.api,
+			ctx:        testUser1.UserCtx,
+		},
+		{
+			name:        "OBJECT already deleted, not found",
+			idToDelete:  OBJECT1.ID,
+			client:      suite.client.api,
+			ctx:         testUser1.UserCtx,
+			expectedErr: "OBJECT not found",
+		},
+		{
+			name:       "happy path, delete OBJECT using personal access token",
+			idToDelete: OBJECT2.ID,
+			client:     suite.client.apiWithPAT,
+			ctx:        context.Background(),
+		},
+		{
+			name:        "unknown OBJECT, not found",
+			idToDelete:  ulids.New().String(),
+			client:      suite.client.api,
+			ctx:         testUser1.UserCtx,
+			expectedErr: notFoundErrorMsg,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("Delete "+tc.name, func(t *testing.T) {
+			resp, err := tc.client.DeleteOBJECT(tc.ctx, tc.idToDelete)
+			if tc.expectedErr != "" {
+				require.Error(t, err)
+				assert.ErrorContains(t, err, tc.expectedErr)
+				assert.Nil(t, resp)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+			assert.Equal(t, tc.idToDelete, resp.DeleteOBJECT.DeletedID)
+		})
+	}
+}
